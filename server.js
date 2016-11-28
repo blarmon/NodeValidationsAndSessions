e3c2fb8b7670f9005693e87a63d4c3fb5ebf177a
Chris Siegel <c.siegel1991@gmail.com>
2016-11-28 23:39:42 +0000

    successfully added email and password validation and used handlebars to conditionally express the error and success messages.

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express'),
     path = require('path'),
     app = express(),
     bodyParser = require("body-parser"),
-    hbs = require('express-handlebars');
+    hbs = require('express-handlebars'),
+    expressSession = require('express-session'),
+    expressValidator = require('express-validator');
 
 //set up my templating engine and views directory
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
@@ -12,24 +14,35 @@ app.set('view engine', 'hbs');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(expressValidator());
+app.use(expressSession( { secret: "chris", saveUninitialized: false, resave: false } ));
 
 app.get('/', function(req, res, next) {
-    res.render('index', { title: "title handled by handlebars", myCondition: true, myArray: [1,3,6,10] });
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+  req.session.errors = null;
 });
 
-app.get('/test/:id', function(req, res, next) {
-   res.end('You accessed this page via a GET request. \n id: ' + req.params.id); 
-});
+app.post('/submit', function(req, res, next) {
+  req.check('email', 'Invalid email address').isEmail();
+  req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
 
-app.post('/test/submit', function(req, res, next) {
-    console.log('id: ' + req.body.id);
-    var id = req.body.id;
-    res.redirect('test/' + id);
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  } else {
+    req.session.success = true;
+  }
+  res.redirect('/');
 });
 
 
 //404 handling!
 
+app.use(function(req, res, next) {
+    res.status(404);
+    res.end('route is not defined, sorry.')
+})
 
 app.listen(8080, function() {
     console.log('listening on 8080');
